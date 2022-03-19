51f6affd556f100f417ae5ad493eb9152477ffae
Hyun Su <tlsgustn93@naver.com>
2022-03-19 21:12:31 +0900

    0319

---
+++ 03.19/수찾기/App.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
+
+const arr1 = input[1].split(' ')
+const arr2 = input[3].split(' ')
+
+let arr = new Set(arr1);
+
+let result = arr2.map(v => arr.has(v) ? 1 : 0)
+
+console.log(result.join('\n'))
+
+// VSC에서는 arr1의 경우 개행문자가 계속 끝에 남아 있어, join('').trim().split(' ')을 해줘야 원형을 유지해줘야 답이 나왔으나
+// 백준에서는 해당사항 없음.
+
