8f9637baea62536ca7caa8911369317f2803e5aa
Hyun Su <tlsgustn93@naver.com>
2022-03-23 13:30:26 +0900

    0323

--- 03.22/스택수열/App.js
+++ 03.22/스택수열/App.js
@@ -1,24 +1,24 @@
-const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n')
+    const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n')
 
-const [n, ...num] = input;
+    const [n, ...num] = input;
 
-const numbers = num.map(v => +v);
+    const numbers = num.map(v => +v);
 
-const stack = [];
-let answer = '';
-let count = 1;
+    const stack = [];
+    let answer = '';
+    let count = 1;
 
-for(let i=0; i<n; i++) {
-    const number = numbers.shift();
-    while(count <= number) {
-        stack.push(count++);
-        answer += '+ ';
+    for(let i=0; i<n; i++) {
+        const number = numbers.shift();
+        while(count <= number) {
+            stack.push(count++);
+            answer += '+ ';
+        }
+        if(stack.pop() !== number) {
+            answer = 'NO'
+            break;
+        }
+        answer += '- '
     }
-    if(stack.pop() !== number) {
-        answer = 'NO'
-        break;
-    }
-    answer += '- '
-}
 
-console.log(answer.split(' ').join('\n'))
+    console.log(answer.split(' ').join('\n'))
